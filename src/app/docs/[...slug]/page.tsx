1507dbb0251f7b1c04ca6769ffdd9386883289b1
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-04-21 09:52:51 +0200

    6.Catch all Segments

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+
+// docs/[...slug]=> mean catch any url that base is /docs 
+
+const Docs = ({params}:any) => {
+    console.log(params)
+    return (
+        <div>slug</div>
+    )
+}
+
+export default Docs
---
+++ src/app/docs/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Docs = () => {
+
+    return (
+        <div>Docs</div>
+    )
+}
+
+export default Docs
