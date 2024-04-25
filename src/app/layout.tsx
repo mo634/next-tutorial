56b51526eacf00612c3a12bce6511f661aabfee8
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-04-25 09:42:01 +0200

    nested layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+
+      <body>
+        <header>Header </header>
+        {children}
+        <footer>footer</footer>
+        </body>
     </html>
   )
 }
