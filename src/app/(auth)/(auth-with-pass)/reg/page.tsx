ab53c400c51c7a1f84b4a74215219a7bd7caa234
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-04-25 09:51:30 +0200

    nested layout with group routing

---
+++ src/app/(auth)/(auth-with-pass)/reg/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reg = () => {
+    return (
+        <div>register</div>
+    )
+}
+
+export default Reg
---
+++ src/app/(auth)/forget-pass/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgetPass = () => {
+    return (
+        <div>page</div>
+    )
+}
+
+export default ForgetPass
